792b3790801f97c41d8e5d3cfc7e106423134d0f
Ruchira tharupathi <ruchiratharupathi01@gmail.com>
2024-10-14 00:09:32 +0530

    test

---
+++ app/src/main/java/sliit/ruchira/todoapp/database/repositories/TodoRepository.kt
@@ -0,0 +1,14 @@
+package sliit.ruchira.todoapp.database.repositories
+
+import sliit.ruchira.todoapp.database.TodoDatabase
+import sliit.ruchira.todoapp.model.Todo
+
+class TodoRepository(private val database:TodoDatabase) {
+
+    fun get(id:Int) = database.getDao().get(id)
+    suspend fun update(id:Int,title:String,description:String,priority:String,deadLineDate:Long) = database.getDao().update(id,title,description,priority,deadLineDate)
+    suspend fun insert(todo:Todo) = database.getDao().insert(todo)
+    suspend fun delete(todo:Todo) = database.getDao().delete(todo)
+    fun getAllTodo():List<Todo> = database.getDao().getAll()
+
+}
